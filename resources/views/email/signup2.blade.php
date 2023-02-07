2d31868fb217eb1a05e54c842ed0ff0d6a8a3287
Victor <vctroseji@gmail.com>
2023-02-07 13:14:19 +0100

    updates

---
+++ resources/views/email/signup2.blade.php
@@ -0,0 +1,12 @@
+<h2>ODT IMS Software</h2><br/><br/>
+
+Hi {{$first_name}} {{$last_name}} 👋, <br/><br/>
+
+A new account has been created for you on our the platform. Use the details below to login and change your password:<br/><br/>
+
+Username: {{$phone_number}}<br/>
+Email: {{$email}}<br/>
+Password: {{$password}}<br/><br/>
+
+<a href="https://ims.gogetit.com.ng">Click here to go to login page</a><br/>
+&copy; 2023. ODT Softs Ltd.
